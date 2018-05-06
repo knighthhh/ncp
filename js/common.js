a474bcc7562aca518527b494d419c1c7fd9d067c
knighthhh <775340093@qq.com>
2018-05-07 00:54:01 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,11 @@
+var serverName = '123.207.12.189';
+var serverPath = '/ncpapp/index.php/';
+//判断是否登录了
+//function islogin(){
+//  var user_phone = plus.storage.getItem('user_phone');
+//  if(user_phone != null){
+//      return true;
+//  }else{
+//     	return false;
+//  }
+//}
